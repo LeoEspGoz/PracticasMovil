d3ba9c77b5e1dfaaf3f465e12d4924c64aeda26d
LeoEspGoz <s19120002lumnos.itsur.edu.mx>
2024-10-06 17:49:01 -0600

    ultimo commit con viewModel implementado y listas

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -9,39 +9,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier){
-    Column( modifier= modifier.padding(16.dp)){
-    var count by remember { mutableStateOf(0)}
-        if(count>0) {
-            var showTask by remember { mutableStateOf(true) }
-            if(showTask){
-                WellnessTaskItem(
-                    onClose = {showTask= false },
-                    modifier = Modifier.padding(top = 16.dp),
-                    taskName= "Have you taken your 15 minutes walk today"
-                    )
-            }
-            Text(
-                text = "You've had $count glasess.",
-                modifier = modifier.padding(16.dp)
-            )
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text("You've had $count glasses.")
         }
-        Row(Modifier.padding(top = 8.dp)){
-        Button(onClick = { count++}, Modifier.padding(top = 16.dp), enabled = count < 10) {
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
             Text("Add one")
         }
-            Button(onClick = {count=0},
-                Modifier.padding(top = 16.dp, start = 8.dp)){
-            Text("Clear Water count")
-
-
-                
-            }
-        }
     }
+}
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(count, { count++ }, modifier)
+
 }
